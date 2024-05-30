8e2036bc0d75f5653a9192deea4d41be188bacc0
WFZ1 <pavelskumenko@gmail.com>
2024-05-30 11:18:50 +0300

    feat: integrate all micro fe into host app with vite module federation

--- task-auth/vite.config.ts
+++ task-auth/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        federation({
+            name: 'task-auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './TaskAuth': './src/App.tsx',
+            },
+            shared: ['react', 'react-dom'],
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: false,
+    },
+});
--- task-host/vite.config.ts
+++ task-host/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        federation({
+            name: 'task-host',
+            remotes: {
+                taskAuth: 'http://localhost:5001/assets/remoteEntry.js',
+                taskEditor: 'http://localhost:5002/assets/remoteEntry.js',
+                taskList: 'http://localhost:5003/assets/remoteEntry.js',
+            },
+            shared: ['react', 'react-dom'],
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: false,
+    },
+});
