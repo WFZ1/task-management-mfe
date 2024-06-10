dff124b30a2291287da3e7d4c879025be4c764d6
WFZ1 <pavelskumenko@gmail.com>
2024-06-10 21:32:26 +0300

    feat: implement redirect from create task to edit task page

--- task-editor/src/routes/create-task.tsx
+++ task-editor/src/routes/create-task.tsx
@@ -1,5 +1,18 @@
 import { TaskFormControl } from '@/components/create-task/task-form-control';
+import { useNavigation } from '@/services/navigation/context';
+import { useEffect } from 'react';
 
 export default function CreateTaskPage() {
+    const { navigate } = useNavigation();
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const taskId = searchParams.get('id');
+
+        if (taskId) {
+            navigate({ to: `/edit-task?id=${taskId}` });
+        }
+    }, [navigate]);
+
     return <TaskFormControl />;
 }
