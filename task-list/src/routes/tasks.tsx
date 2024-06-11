11ac990ffedb9e88be5d1ed56c50e6cb51586a36
WFZ1 <pavelskumenko@gmail.com>
2024-06-11 09:19:37 +0300

    fix: make tasks visible on host mfe

--- task-list/src/routes/tasks.tsx
+++ task-list/src/routes/tasks.tsx
@@ -2,8 +2,9 @@ import { Header } from '@/components/tasks/header';
 import { Tasks } from '@/components/tasks/tasks';
 
 export default function TasksPage() {
+    // TODO: investigate why 'hidden' class for div below works differently from host app
     return (
-        <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
             <Header />
             <Tasks />
         </div>
